46378a8e86b3997b198f455434071360ced7e0e7
gadooo <mogdad19998@gmail.com>
2024-01-18 12:35:01 +0200

    create custom list view for notes

---
+++ lib/views/widgets/custom_note_list_view.dart
@@ -0,0 +1,18 @@
+import 'package:easy_note/views/widgets/custom_note_item.dart';
+import 'package:flutter/material.dart';
+
+class CustomNotesListView extends StatelessWidget {
+  const CustomNotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: NoteItem(),
+          );
+        });
+  }
+}
--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:easy_note/views/widgets/custom_app_bar.dart';
 import 'package:easy_note/views/widgets/custom_note_item.dart';
+import 'package:easy_note/views/widgets/custom_note_list_view.dart';
 import 'package:flutter/material.dart';
 
 class NoteviewBody extends StatelessWidget {
@@ -9,13 +10,8 @@ class NoteviewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.all(16.0),
-      child: Column(children: [
-        Padding(
-          padding: EdgeInsets.only(top: 30.0, bottom: 16),
-          child: CustomAppBar(),
-        ),
-        NoteItem()
-      ]),
+      child: Column(
+          children: [CustomAppBar(), Expanded(child: CustomNotesListView())]),
     );
   }
 }
