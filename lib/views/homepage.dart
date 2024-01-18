37305adcd5fb2aaf9ce10d3c15376b7af1193fee
gadooo <mogdad19998@gmail.com>
2024-01-18 13:03:40 +0200

    show model sheet

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:easy_note/views/widgets/add_note_buttom_sheet.dart';
 import 'package:easy_note/views/widgets/note_view_body.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteButtmSheet();
+              });
+        },
         child: const Icon(Icons.add),
       ),
       body: const NoteviewBody(),
