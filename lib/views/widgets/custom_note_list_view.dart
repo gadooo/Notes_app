d936e2cae250d524e4cc5aaaabf358711f1d1962
gadooo <mogdad19998@gmail.com>
2024-01-18 12:56:37 +0200

    change the padding of note list view

--- lib/views/widgets/custom_note_list_view.dart
+++ lib/views/widgets/custom_note_list_view.dart
@@ -6,13 +6,17 @@ class CustomNotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 4.0),
-            child: NoteItem(),
-          );
-        });
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0),
+              child: NoteItem(),
+            );
+          }),
+    );
   }
 }
