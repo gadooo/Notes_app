43a6da8a89323ae6679ef4a81e02079d772185f2
gadooo <mogdad19998@gmail.com>
2024-01-20 15:21:13 +0200

    add the functionsal add notes logic

--- lib/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:easy_note/constans.dart';
 import 'package:easy_note/models/note_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
-  addNote(NoteModeel note) {}
+  addNote(NoteModeel note) async {
+    emit(AddNoteCubitLoading());
+
+    try {
+      emit(AddNoteCubitSucces());
+      var noteBox = Hive.box(knotebox);
+      await noteBox.add(note);
+    } catch (e) {
+      AddNoteCubitFailure(reeMessafe: e.toString());
+    }
+  }
 }
