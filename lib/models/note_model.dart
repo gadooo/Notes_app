d2cc6e81963a6cc8458f91fc96313eb9d6563c61
gadooo <mogdad19998@gmail.com>
2024-01-20 08:18:50 +0200

    gnreate hive objects

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NoteModeel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModeel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final DateTime dateTime;
+  @HiveField(3)
   final int color;
 
   NoteModeel(
